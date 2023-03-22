428887eba7e4c4479d734834d5e59c9fe0824969
DF <ds.fai@outlook.com>
2023-03-22 11:27:09 -0700

    5.5.20 Updated the front end App.jsx file to support calls to the new backend address

--- frontend/myapp/src/App.jsx
+++ frontend/myapp/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
   function getData() {
     axios({
       method: "GET",
-      url: "https://shopsite.herokuapp.com/",
+      url: "https://fathomless-bayou-14966.herokuapp.com/",
     })
     .then((response) => {
       const res =response.data
@@ -63,7 +63,7 @@ function App() {
 
     // This is how to do an Axios POST.  Notice the route first, then the dictionary body.  Also pay attention to the routes during testing.  You may have to swap it out for your local routes.
     function signIn() {
-      axios.post('https://shopsite.herokuapp.com/dbPost', {
+      axios.post('https://fathomless-bayou-14966.herokuapp.com/dbPost', {
         name: 'David'
       })
       .then((response) => {
@@ -98,7 +98,7 @@ function App() {
 
 // backtick dollar sign to get valuestring
     function SignInToDB() {
-      axios.post('https://shopsite.herokuapp.com/signIn', {
+      axios.post('https://fathomless-bayou-14966.herokuapp.com/signIn', {
         name: `${UserName}`
       })
       .then((response) => {
