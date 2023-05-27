ab7eacec3c82bdbfe0e415b1b33b8d47a57b9d48
DF <ds.fai@outlook.com>
2023-05-27 15:05:07 -0700

    7.9.33 Added a new ui for the discussion page included a progression bar added buttons to the grid and arranged them in a better way added an image to the grid resized the image and gave it a max height added percentages to the width and height of the image added a subject to the top of the grid brought in bootstrap bars with animation

--- frontend/myapp/src/pages_discussion/Discussion.jsx
+++ frontend/myapp/src/pages_discussion/Discussion.jsx
@@ -1,6 +1,31 @@
+import './Discussion.css'
+import ProgressBar from 'react-bootstrap/ProgressBar';
+
 const Discussion = () => {
-    return <h1>Let's Start a Discussion</h1>;
-  };
+    return (
+    <div>
+    <h1>Let's Start a Discussion</h1>
+    <form>
+        <input type='date'/>
+    </form>
+    <div class='grid-container'>
+        <h1 class='grid-item item-h'>SUBJECT</h1>
+        <img class='grid-item item-g' src={process.env.PUBLIC_URL + './machineLearningImage.PNG'}/>
+        <ProgressBar class ='grid-item item-i' striped variant="info" key={1} animated now={1000} label={"Time Left"}/>
+        <button class='grid-item item-a'>Begin</button>
+        <button class='grid-item item-b'>Next Subject</button>
+        <button class='grid-item item-c'>Go Back</button>
+        <button class='grid-item item-d'>End</button>
+        <button class='grid-item item-e'>Save</button>
+        <button class='grid-item item-f'>Help</button>
+    </div>
+
+
+
+    </div>
+    )};
   
   export default Discussion;
   
+  // Update the documentation on how the routing was setup and the package you needed to do it.
+  // Make sure to include the file structure on PAPER or on a Diagram and save.
