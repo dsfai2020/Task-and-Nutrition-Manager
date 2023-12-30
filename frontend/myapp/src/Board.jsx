d4b8be1a56b1ee967a72550c7a31da1ca02c0ee9
DF <ds.fai@outlook.com>
2023-12-30 05:10:25 -0800

    Updated formulas

--- frontend/myapp/src/Board.jsx
+++ frontend/myapp/src/Board.jsx
@@ -52,23 +52,86 @@ const Board = () => {
   }, [columns]);
 
   const handleDragEnd = (result) => {
-    // ... (unchanged)
+    const { source, destination } = result;
+
+    if (!destination) return;
+
+    if (
+      source.droppableId === destination.droppableId &&
+      source.index === destination.index
+    ) {
+      return;
+    }
+
+    const startColumn = columns[source.droppableId];
+    const endColumn = columns[destination.droppableId];
+
+    if (startColumn === endColumn) {
+      const newItems = Array.from(startColumn.items);
+      const [removed] = newItems.splice(source.index, 1);
+      newItems.splice(destination.index, 0, removed);
+
+      const newColumn = {
+        ...startColumn,
+        items: newItems,
+      };
+
+      setColumns((prevColumns) => ({
+        ...prevColumns,
+        [newColumn.id]: newColumn,
+      }));
+    } else {
+      const startItems = Array.from(startColumn.items);
+      const endItems = Array.from(endColumn.items);
+
+      const [removed] = startItems.splice(source.index, 1);
+      endItems.splice(destination.index, 0, removed);
+
+      const newStartColumn = {
+        ...startColumn,
+        items: startItems,
+      };
+
+      const newEndColumn = {
+        ...endColumn,
+        items: endItems,
+      };
+
+      setColumns((prevColumns) => ({
+        ...prevColumns,
+        [newStartColumn.id]: newStartColumn,
+        [newEndColumn.id]: newEndColumn,
+      }));
+    }
   };
 
   const handleAddCard = (columnId) => {
-    // ... (unchanged)
+    const newCardId = `item-${Date.now()}`;
+    const newCard = { id: newCardId, content: 'New Card' };
+
+    setColumns((prevColumns) => {
+      const newColumns = { ...prevColumns };
+      newColumns[columnId].items.push(newCard);
+      return newColumns;
+    });
   };
 
   const handleItemNameChange = (columnId, itemId, newName) => {
-    // ... (unchanged)
+    setColumns((prevColumns) => {
+      const newColumns = { ...prevColumns };
+      newColumns[columnId].items = newColumns[columnId].items.map((item) =>
+        item.id === itemId ? { ...item, content: newName } : item
+      );
+      return newColumns;
+    });
   };
 
   const handleItemClick = (columnId, itemId) => {
-    // ... (unchanged)
+    setEditingItem({ columnId, itemId });
   };
 
   const handleItemBlur = () => {
-    // ... (unchanged)
+    setEditingItem(null);
   };
 
   return (
