43014527adf53407a2a0d534ee59438e3ccd3fe2
ds.fai2020 <ds.fai@outlook.com>
2024-05-30 19:47:01 -0700

    v2.0.5-alpha Added story point, size and value as circular input text buttons that display next to labels.  Created labels for each of them that sit in a column.  Customized the css containers and some of the flexbox properties.  Fine tuned some of the design to focus on the mobile display of how the site looks.

--- frontend/myapp/src/StoryUi.jsx
+++ frontend/myapp/src/StoryUi.jsx
@@ -15,15 +15,26 @@ function StoryUiComponent (props) {
     // custom states
     const [someState, setsomeState] = useState(1)
 
+    // consider making more ui sub-components related to this specific component (like buttons for the StoryUiComponent) here for reusability as well. 
+
     return (
         <div class='storyUi-Container'>
             <textarea class='text-a' type='text' placeholder='Name the story'></textarea>
             <textarea class='text-b' type='text' placeholder='Describe the story'></textarea>
+
+            <div class='label-Container'>
+                <p>Estimate</p>
+                <p>Value</p>
+                <p>Size</p>
+            </div>  
+
             <div class='metric-Container'>
-                <button class='metric-a'>size</button>
-                <button class='metric-b'>value</button>
-                <button class='metric-c'>label</button>
+                <input type='text' id='textInput' placeholder='0' class='metric-a'></input>
+                <input type='text' id='textInput-2' placeholder='0' class='metric-b'></input>
+                <input type='text' id='textInput-3' placeholder='0' class='metric-c'></input>
             </div>
+
+
         </div>
             )
 };
