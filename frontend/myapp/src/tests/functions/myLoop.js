04273b0412b2d0c262b57b670f16fe4959934233
ds.fai2020 <ds.fai@outlook.com>
2024-06-13 05:03:24 -0700

    v2.3.3-alpha completed small unit test related to mapping in order to prepare for the Ui being loaded from local storage

---
+++ frontend/myapp/src/tests/functions/myLoop.js
@@ -0,0 +1,11 @@
+const myLoop = (item) => {
+    const x=['a','b','c']
+    // Remember that when mapping, it will run the function for each iteration through the item being mapped
+    x.map((something)=>{
+        item.push(something)
+    });
+    console.log(item);
+    return item
+};
+
+module.exports = myLoop;
