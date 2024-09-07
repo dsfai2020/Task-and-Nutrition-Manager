e674da366981bf96be6a66e32d2aac847e94af9a
ds.fai2020 <ds.fai@outlook.com>
2024-09-06 17:56:11 -0700

    v2.6.1 added minor text update to schedule ui.

--- frontend/myapp/src/Schedule.jsx
+++ frontend/myapp/src/Schedule.jsx
@@ -1,17 +1,20 @@
 import {useState, setState, useEffect} from 'react';
-
+import '/.Schedule.css'
 export default function Schedule(props) {
     return (
         <div>
             <p>SCHEDULE</p>
-            <ScheduleUI />
+            <ScheduleUI morning='7am to Noon'/>
+            <ScheduleUI morning='1pm to 5pm'/>
+            <ScheduleUI morning='6pm to Midnight'/>
         </div>
     );
 
     function ScheduleUI (props) {
         return (
             <div>
-                <p>Schedule UI Temporary Holder</p>
+                <button class='scheduleButton'></button>
+                <p>Time {props.morning} </p>
             </div>
         )
     };
