ddca7fcf1f64ccf5d03c4c58ebde69e1940f51f0
ds.fai2020 <ds.fai@outlook.com>
2024-12-08 22:00:09 -0800

    v3.2.12 Minor text update

--- frontend/myapp/src/pages/Home.jsx
+++ frontend/myapp/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import './Home.css'
 const Home = () => {
     return (
     <div class='Container'>
-      <p class='Version-Control'>Version 3.2.10 Alpha - Last Updated: Dec 5, 2024 </p>
+      <p class='Version-Control'>Version 3.2.12 Alpha - Last Updated: Dec 8, 2024 </p>
     </div>
     )
     ;
