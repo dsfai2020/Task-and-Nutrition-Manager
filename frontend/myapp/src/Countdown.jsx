20b298712d988a8490aed743676b9dbeb16e14c7
ds.fai2020 <ds.fai@outlook.com>
2025-04-06 16:48:26 -0700

    dev-counter-v0.0.2 Fixed an issue where the start button was causing duplicate timers to launch.  This would create floating intervals that leaked without any mounting points for shutoff.  This caused unbound valves to open up timers.  I separated these into their own effect and made the interval run on conditionals based on the state of the timer.  I also made that timer state the trigger point for the effect.  It had 2 total if statements bound to it so that one or the other would take over (start or clear).

--- frontend/myapp/src/Countdown.jsx
+++ frontend/myapp/src/Countdown.jsx
@@ -6,29 +6,33 @@ export default function Countdown (props) {
 
     const [timerStatus, setTimerStatus] = useState('off')
 
+    
+    // Originally I placed the intervalId in the HandleTimerWhenOff function.  But I brought it up to the top level here to make it more accessible without having it trigger a rerender.
+    let intervalId = 'bay 1'
+    
+
+    // This is a cleaner way to do it instead of using functions.  You create 2 if statements that trigger whenever the timerStatus states change.  They will work cooperatively as on/off valves and override each other.  Since they are in the same effect, they'll be able to recognize the same intervalId a lot easier - preventing leaks.  It will operate with 2 switch buttons that will alter the state of timerStatus.
+    useEffect(()=>{
+        if (timerStatus==='on'){ 
+            intervalId = setInterval(()=>{
+            // each interval run this 1000 miliseconds
+            console.log('Counted')
+            }, 1000);
+            };
+        if (timerStatus==='off'){
+            clearInterval(intervalId)
+            console.log('clearing in effect')
+        }        
+    }, [timerStatus])
+
     function HandleTimerWhenOff(props) {
         // Just like python classes you can sort of make a class bound variable without actually using a class.  The function itself in React works as a sort of 'class' that becomes rendered.  It has its own properties that need to be drilled down.
-        let intervalId = 'bay 1'
-
+        
         function StartTimer (){
             // If timerStatus is off lets turn on the timer.
             setTimerStatus('on')
             console.log('Starting timer pressed')
-            if (timerStatus==='on'){            
-                intervalId = setInterval(()=>{
-                // each interval run this 1000 miliseconds
-                console.log('Counted')
-                }, 1000);}
             }
-
-        function ClearTimer (props){
-            clearInterval(intervalId);
-            console.log('clearing interval');
-        }
-
-        function MyCustomButton (props){
-            return <button onClick={ClearTimer}>CUSTOM BUTTON{props.custom}</button>
-        }
 
         function MyOtherCustomButton (props) {
             return <button onClick={StartTimer}>CUSTOM START</button>
@@ -37,11 +41,29 @@ export default function Countdown (props) {
         return (
             <div>
                 {/* All you got to do now is make it so that These conditionally render {() ? :} */}
-                <MyCustomButton custom='true2'/>
                 <MyOtherCustomButton />
             </div>
         )
     };
+
+    function HandleTimerWhenOn () {
+
+        function ClearTimer (props){
+            clearInterval(intervalId);
+            console.log('clearing interval');
+            setTimerStatus('off')
+        }
+
+        function MyCustomButton (props){
+            return <button onClick={ClearTimer}>CUSTOM BUTTON{props.custom}</button>
+        }
+
+        return (
+            <div>
+                <MyCustomButton custom='true2'/>
+            </div>
+        )
+    }
 
     return (
         <div class='Countdown-container'>
@@ -50,6 +72,9 @@ export default function Countdown (props) {
                 : <button onClick={HandleTimerWhenOff}>Stop</button>}  */}
             
             <HandleTimerWhenOff />
+
+            {/* Start button */}
+            <HandleTimerWhenOn />
             <p>Time Remaining</p>
             
             <p>This is where the countdown UI will go</p>
