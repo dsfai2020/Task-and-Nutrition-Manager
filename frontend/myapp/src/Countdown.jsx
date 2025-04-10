372bbcf68fe272159bb78490e34a89641179b112
ds.fai2020 <ds.fai@outlook.com>
2025-04-10 05:48:13 -0700

    dev-counter-v0.0.4 The timer will show the amount of time that has elapsed.  Fixed memory leak issues.

--- frontend/myapp/src/Countdown.jsx
+++ frontend/myapp/src/Countdown.jsx
@@ -4,26 +4,40 @@ import {useState, setState, useEffect} from 'react';
 
 export default function Countdown (props) {
 
-    const [timerStatus, setTimerStatus] = useState('off')
-
-    
+  
     // Originally I placed the intervalId in the HandleTimerWhenOff function.  But I brought it up to the top level here to make it more accessible without having it trigger a rerender.
     let intervalId = 'bay 1'
-    
+
 
     // This is a cleaner way to do it instead of using functions.  You create 2 if statements that trigger whenever the timerStatus states change.  They will work cooperatively as on/off valves and override each other.  Since they are in the same effect, they'll be able to recognize the same intervalId a lot easier - preventing leaks.  It will operate with 2 switch buttons that will alter the state of timerStatus.
-    useEffect(()=>{
-        if (timerStatus==='on'){ 
-            intervalId = setInterval(()=>{
-            // each interval run this 1000 miliseconds
-            console.log('Counted')
-            }, 1000);
+
+
+    const [timerStatus, setTimerStatus] = useState('off')
+    const [count, setCount] = useState(1)
+
+    function MyHook () {
+        
+        useEffect(()=>{
+            if (timerStatus==='on'){
+                intervalId = setInterval(()=>{
+                // each interval is 1000 miliseconds and runs all the code here.
+                console.log('Counted '+ count);  
+                setCount(count+1)
+                }, 1000);
             };
-        if (timerStatus==='off'){
-            clearInterval(intervalId)
-            console.log('clearing in effect')
-        }        
-    }, [timerStatus])
+
+            return () => {clearInterval(intervalId)}
+
+        }, [timerStatus]);
+
+        return (
+            <div>
+                <p>Time in Progress: {count}</p>
+            </div>
+        )
+
+    }
+
 
     function HandleTimerWhenOff(props) {
         // Just like python classes you can sort of make a class bound variable without actually using a class.  The function itself in React works as a sort of 'class' that becomes rendered.  It has its own properties that need to be drilled down.
@@ -35,13 +49,13 @@ export default function Countdown (props) {
             }
 
         function MyOtherCustomButton (props) {
-            return <button onClick={StartTimer}>CUSTOM START</button>
+            return <button onClick={StartTimer}>{props.name}</button>
         }
 
         return (
             <div>
                 {/* All you got to do now is make it so that These conditionally render {() ? :} */}
-                <MyOtherCustomButton />
+                <MyOtherCustomButton name='START'/>
             </div>
         )
     };
@@ -49,18 +63,18 @@ export default function Countdown (props) {
     function HandleTimerWhenOn () {
 
         function ClearTimer (props){
-            clearInterval(intervalId);
             console.log('clearing interval');
             setTimerStatus('off')
+            // to pause the timer do not set the count back to zero.  Instead do nothing to it.
         }
 
         function MyCustomButton (props){
-            return <button onClick={ClearTimer}>CUSTOM BUTTON{props.custom}</button>
+            return <button onClick={ClearTimer}>{props.name}</button>
         }
 
         return (
             <div>
-                <MyCustomButton custom='true2'/>
+                <MyCustomButton name='STOP'/>
             </div>
         )
     }
@@ -73,10 +87,9 @@ export default function Countdown (props) {
                 // Stops the timer
                 : <HandleTimerWhenOn />
                 } 
-
-            <p>Time Remaining</p>
             
             <p>This is where the countdown UI will go</p>
+            <MyHook />
         </div>
     )
 
