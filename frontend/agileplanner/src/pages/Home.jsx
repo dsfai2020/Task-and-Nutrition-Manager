cf5c4a3142bd5e54ffaf55fd3973fde235e94c6c
ds.fai2020 <ds.fai@outlook.com>
2025-05-27 17:06:59 -0700

    v4.0.7 Hotfix in navbar

--- frontend/agileplanner/src/pages/Home.jsx
+++ frontend/agileplanner/src/pages/Home.jsx
@@ -3,11 +3,7 @@ import './Home.css'
 const Home = () => {
     return (
     <div class='Container'>
-<<<<<<< HEAD
-      <p class='Version-Control'>Version 4.0.4 Alpha - Last Updated: May 27, 2025 </p>
-=======
-      {/* <p class='Version-Control'>Version 3.3.49 Alpha - Last Updated: Mar 29, 2025 </p> */}
->>>>>>> dev
+      {/* <p class='Version-Control'>Version 4.0.4 Alpha - Last Updated: May 27, 2025 </p> */}
     </div>
     )
     ;
