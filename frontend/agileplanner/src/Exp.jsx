8aaabf4e22bf3e49eb1a7a654c2dc6635ff426d9
ds.fai2020 <ds.fai@outlook.com>
2025-07-02 18:47:20 -0700

    v4.0.18 Updated site layout for navbar.  Made changes to the css for the schedule in mobile view.  Made changes to the css in desktop view.

--- frontend/agileplanner/src/Exp.jsx
+++ frontend/agileplanner/src/Exp.jsx
@@ -26,10 +26,10 @@ export function ExpComponent(){
                 <h1 class='Exp-Fill' style={{width: `${exp}%`, backgroundColor: 'blue'}}></h1>
                 <h1 style={{zIndex: '99', height: '1vh', fontSize: '15px', alignSelf: 'center'}}>{exp} EXP</h1>
             </div>
-
+{/* 
             <div>
                 <input class='exp-text-area' placeholder='coming soon' onChange={handleExp} value={exp}></input>
-            </div>
+            </div> */}
 
         </div>
     )
